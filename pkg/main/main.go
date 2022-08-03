5da05567f5b125f54b50b152a0347ef3868afa22
Hernan Alejandro Cristaldo <hac2794@gmail.com>
2022-08-03 16:56:21 -0300

    update code 1.3

--- pkg/main/main.go
+++ pkg/main/main.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"github.com/HernAle/AvataredBeta/pkg/avatar"
-	"github.com/HernAle/AvataredBeta/pkg/avatar/encoder"
-	"github.com/HernAle/AvataredBeta/pkg/avatar/images"
 )
 
 func main() {
-	enc := encoder.NewMD5Encoder()
-	imgGen := images.NewAvatar()
-
-	avt := avatar.NewService(enc, imgGen)
-	avt.GenerateAndSaveAvatar("lara@gmail.com")
+	avt := avatar.NewService()
+	avt.GenerateAndSaveAvatar("anybody@email.com")
 }
